b3d1c911aa39c66a930f95bc9c2fa5bc3ce14768
Martina Doekharan <kmd98x@hotmail.com>
2024-12-02 21:13:49 +0100

    begin aan js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,46 @@
 // JavaScript Document
-console.log("hi");
+
+
+// https://www.w3schools.com/jsref/jsref_obj_date.asp + chatGPT
+
+// function getNextDay(date) {
+//     // Clone the date object to avoid mutating the original date
+//     let nextDay = new Date(date);
+    
+//     // Add one day to the date
+//     nextDay.setDate(date.getDate() + 1);
+    
+//     return nextDay;
+// }
+
+
+
+// function displayDeliveryDate() {
+//  const delivery_date = document.querySelector("delivery_date");
+
+//  const today = new Date();
+//  const tomorrow = getNextDay(today);
+
+// //  delivery_date.innerHTML = tomorrow;
+//  console.log(tomorrow)
+// }
+
+// displayDeliveryDate();
+
+
+
+
+function footerLinks () {
+    const footerHeadings = document.querySelectorAll("footer .header-link");
+
+    footerHeadings.forEach(heading => {
+        heading.addEventListener("click", () => {
+            console.log("clicked")
+
+            
+        })
+    })
+
+}
+
+footerLinks()
