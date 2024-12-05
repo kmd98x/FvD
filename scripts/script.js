f43899d2c94a4c63c7a14ecc67b5b66621f63084
Martina Doekharan <kmd98x@hotmail.com>
2024-12-05 21:23:11 +0100

    455 goed

--- scripts/script.js
+++ scripts/script.js
@@ -42,4 +42,18 @@ listHeadings.forEach((heading) => {
 
 
 
+const heart = document.querySelector('.heart-count');
+const button = document.querySelectorAll('button');
 
+button.forEach((button) => {
+	button.addEventListener('click', () => {
+		if (button.classList.contains('liked')) {
+			heart.innerText = parseInt(heart.innerText) - 1;
+			button.classList.remove('liked');
+		} else {
+			heart.innerText = parseInt(heart.innerText) + 1;
+			button.classList.add('liked');
+		}
+	});
+})
+heart.innerText = '0';
